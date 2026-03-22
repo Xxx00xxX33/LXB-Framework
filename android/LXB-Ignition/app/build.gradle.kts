3a51fdc37cb2e7a20dd506b831e9ad786de099c5
陈润果 <1415327394@qq.com>
2026-03-22 19:50:04 +0800

    chore(release): cut v0.2.4

--- android/LXB-Ignition/app/build.gradle.kts
+++ android/LXB-Ignition/app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.example.lxb_ignition"
         minSdk = 30
         targetSdk = 36
-        versionCode = 203
-        versionName = "0.2.3"
+        versionCode = 204
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
