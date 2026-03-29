43a6c89750fc42a7eff162c05ee388e76e44079f
WuWei <1415327394@qq.com>
2026-03-29 20:27:08 +0800

    chore(release): cut v0.4.7

--- android/LXB-Ignition/app/build.gradle.kts
+++ android/LXB-Ignition/app/build.gradle.kts
@@ -192,8 +192,8 @@ android {
         applicationId = "com.example.lxb_ignition"
         minSdk = 30
         targetSdk = 36
-        versionCode = 406
-        versionName = "0.4.6"
+        versionCode = 407
+        versionName = "0.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
